83f0b15c1695a5f12e8951ff08434f863c7c4f12
stakanov <senatoredaniele@gmail.com>
2024-05-12 19:52:17 +0200

    config_schema

--- esphome/components/cc1101/__init__.py
+++ esphome/components/cc1101/__init__.py
@@ -2,20 +2,25 @@ import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import button
 from esphome.components import spi
-from esphome.const import CONF_ID
+from esphome.const import (CONF_ID)
 
 DEPENDENCIES = ["spi"]
 
 cc1101_ns = cg.esphome_ns.namespace("cc1101")
 CC1101 = cc1101_ns.class_("CC1101", button.Button, cg.Component, spi.SPIDevice)
 
-CONFIG_SCHEMA = (
-    cv.Schema({})
-    .extend(spi.spi_device_schema())
-)
+CONFIG_SCHEMA = cv.Schema(
+    {
+        cv.GenerateID(): cv.declare_id(CC1101),
+    }
+).extend(spi.spi_device_schema())
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
     await spi.register_spi_device(var, config)
     await button.register_button(var, config)
+
+
+####################################################################################################
+
