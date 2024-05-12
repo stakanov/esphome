6183c3a4e6b4657846687ad4fbbb99be40669c75
stakanov <senatoredaniele@gmail.com>
2024-05-12 19:34:28 +0200

    added spi.pins

--- esphome/components/cc1101/cc1101.cpp
+++ esphome/components/cc1101/cc1101.cpp
@@ -7,8 +7,13 @@ namespace cc1101 {
 
 static const char *const TAG = "cc1101.button";
 
-void XPT2046Component::setup() {
-  this->spi_setup();
+static const uint8_t SCK_PIN = 18;
+static const uint8_t MISO_PIN = 19;
+static const uint8_t MOSI_PIN = 23;
+static const uint8_t CS_PIN = 5;
+
+void CC1101::setup() {
+  SPI.pins(SCK_PIN, MISO_PIN, MOSI_PIN, CS_PIN);
 }
 
 void CC1101::dump_config() { 
