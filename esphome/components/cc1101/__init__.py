27e572290aed3899e01c04b90dab368ecc2cc7de
stakanov <senatoredaniele@gmail.com>
2024-05-12 20:00:26 +0200

    __init__.py

--- esphome/components/cc1101/__init__.py
+++ esphome/components/cc1101/__init__.py
@@ -10,17 +10,31 @@ cc1101_ns = cg.esphome_ns.namespace("cc1101")
 CC1101 = cc1101_ns.class_("CC1101", button.Button, cg.Component, spi.SPIDevice)
 
 CONFIG_SCHEMA = cv.Schema(
-    {
+    {   
         cv.GenerateID(): cv.declare_id(CC1101),
+        #cv.Required(CONF_ID): cv.declare_id(CC1101),
     }
-).extend(spi.spi_device_schema())
+).extend(spi.spi_device_schema(cs_pin_required=True))
+
+
+async def to_code(config):
+    var = cg.new_Pvariable(config[CONF_ID])   
+    await cg.register_component(var, config)
+    await button.register_button(var, config)
+    await spi.register_spi_device(var, config)
+
+####################################################################################################
+"""
+ CONFIG_SCHEMA = cv.Schema(
+    {
+        cv.GenerateID(): cv.declare_id(ADS1118),
+    }
+).extend(spi.spi_device_schema(cs_pin_required=True))
+
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
     await spi.register_spi_device(var, config)
-    await button.register_button(var, config)
 
-
-####################################################################################################
-
+"""
